661f5ce4ac42aef0122d88ba34b1681cfbcdd067
Daniel Lindros <daniell@lnxdaniell.se.axis.com>
2014-02-11 13:58:35 +0100

    Added support for ping and pong frames. Increased maximum payload length.

--- WebSocket.cpp
+++ WebSocket.cpp
@@ -1,10 +1,10 @@
 #include "WebSocket.h"
 #include "sha1.h"
 #include "Base64.h"
+#include <StandardCplusplus.h>
+#include <serstream>
 
-//#define DEBUG
-
-
+using namespace std;
 
 struct Frame {
     bool isMasked;
@@ -12,70 +12,74 @@ struct Frame {
     byte opcode;
     byte mask[4];
     byte length;
-    char data[64];
+    char data[MAX_RECEIVE_MESSAGE_SIZE];
 } frame;
-
 
 WebSocket::WebSocket(const char *urlPrefix, int inPort) :
     server(inPort),
-//    client(255),
     socket_urlPrefix(urlPrefix)
 {
     state = DISCONNECTED;
     onConnect = NULL;
     onData = NULL;
     onDisconnect = NULL;
+    onHeartbeatResponse = NULL;
 }
-
 
 void WebSocket::begin() {
-    server.begin();
+    server.begin();      
 }
 
-
 void WebSocket::listen() {
-	EthernetClient cli;
-    if (cli = server.available()) {
-        if (cli == true) {
-            if (state == DISCONNECTED ) {
-				client = cli;
-                if (doHandshake() == true) {
-                    state = CONNECTED;
-                    if (onConnect) {
-                        onConnect(*this);
-                    }
+	
+    client = server.available();
+    
+    if (client == true) {
+        // No active connection available. Treat this as a handshake request.
+        if (state == DISCONNECTED) {
+            bool handshakeSuccessful = processHandshake();
+            
+            if (handshakeSuccessful) {
+                state = CONNECTED;                    
+                // Call the registered callback.
+                if (onConnect) {
+                    onConnect(*this);
                 }
             } else {
-                if (getFrame() == false) {
-                    // Got unhandled frame, disconnect
-	            	#ifdef DEBUG
-	                	Serial.println("Disconnecting");
-	            	#endif
-                    disconnectStream();
-                    state = DISCONNECTED;
-                    if (onDisconnect) {
-                        onDisconnect(*this);
-                    }
-                }
+                cout << F("WEBSOCKET: Handshake unsuccessful.\n");
+            }
+        } 
+        else if (state == CONNECTED || state == CLOSING) {
+            bool validFrame = getFrame();
+
+            // We check the frame to see that it was valid, and disconnect
+            // if it was not.
+            if (!validFrame) {                
+            	cout << F("WEBSOCKET: Got invalid frame. Disconnecting.\n");
+                disconnect();
             }
         }
     }
 }
 
-
 bool WebSocket::isConnected() {
 	return (state == CONNECTED) ? true : false;
 }
 
-
-void WebSocket::disconnectStream() {
+void WebSocket::disconnect() {
     client.flush();
     delay(1);
     client.stop();
+
+    state = DISCONNECTED;
+
+    // Call the registered callback.
+    if (onDisconnect) {
+        onDisconnect(*this);
+    }
 }
 
-
-bool WebSocket::doHandshake() {
+bool WebSocket::processHandshake() {
     char temp[128];
     char key[80];
     char bite;
@@ -88,17 +92,14 @@ bool WebSocket::doHandshake() {
     bool hasKey = false;
 
     byte counter = 0;
+    
+    cout << F("WEBSOCKET: Processing handshake\n");
     while ((bite = client.read()) != -1) {
         temp[counter++] = bite;
 
         if (bite == '\n' || counter > 127) { // EOL got, or too long header. temp should now contain a header string
-            temp[counter - 2] = 0; // Terminate string before CRLF
-            
-            #ifdef DEBUG
-                Serial.print("Got header: ");
-                Serial.println(temp);
-            #endif
-            
+            temp[counter - 2] = 0; // Terminate string before CRLF                    
+
             // Ignore case when comparing and allow 0-n whitespace after ':'. See the spec:
             // http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
             if (!hasUpgrade && strstr(temp, "Upgrade:")) {
@@ -118,14 +119,29 @@ bool WebSocket::doHandshake() {
                 isSupportedVersion = true;
             }
             
+            cout << temp;
             counter = 0; // Start saving new header string
         }
     }
 
+    if (!hasUpgrade)
+        cout << F("Handshake has no \"Upgrade\" entry.");
+    if (!hasConnection)
+        cout << F("Handshake has no \"Connection\" entry.");
+    if (!isSupportedVersion)
+        cout << F("Handshake has no \"Sec-Websocket-Version\" entry.");
+    if (!hasHost)
+        cout << F("Handshake has no \"Host\" entry.");
+    if (!hasOrigin)
+        cout << F("Handshake has no \"Origin\" entry.");
+    if (!hasKey)
+        cout << F("Handshake has no \"Sec-Websocket-Key\" entry.");
+
+
     // Assert that we have all headers that are needed. If so, go ahead and
     // send response headers.
     if (hasUpgrade && hasConnection && isSupportedVersion && hasHost && hasOrigin && hasKey) {
-        strcat(key, "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"); // Add the omni-valid GUID
+        strcat(key, "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"); // Add the omni-valid GUI
         Sha1.init();
         Sha1.print(key);
         uint8_t *hash = Sha1.result();
@@ -148,28 +164,64 @@ bool WebSocket::doHandshake() {
 
 bool WebSocket::getFrame() {
     byte bite;
+    unsigned short payloadLength = 0;
     
-    // Get opcode
-    bite = client.read();
-        
+    bite = client.read();        
     frame.opcode = bite & 0xf; // Opcode
     frame.isFinal = bite & 0x80; // Final frame?
-    // Determine length (only accept <= 64 for now)
+    // Determine length (only accept <= 126, i.e. maximum 65535 bytes. Need to cap this in-code.)
     bite = client.read();
-    frame.length = bite & 0x7f; // Length of payload
-    if (frame.length > 64) {
-        #ifdef DEBUG
-            Serial.print("Too big frame to handle. Length: ");
-            Serial.println(frame.length);
-        #endif
-        client.write((uint8_t) 0x08);
-        client.write((uint8_t) 0x02);
-        client.write((uint8_t) 0x03);
-        client.write((uint8_t) 0xf1);
+    frame.length = bite & 0x7f; // Length of payload        
+    frame.isMasked = bite & 0x80;
+
+    // Frame complete!
+    if (!frame.isFinal) {
+        // We don't handle fragments! Close and disconnect.
+        cout << F("WEBSOCKET: Non-final frames not supported.");
+        close(WS_POLICY_VIOLATION, "Non-final frames not supported.");
+
         return false;
     }
-    // Client should always send mask, but check just to be sure
-    frame.isMasked = bite & 0x80;
+
+    // First check if the frame size is within our limits.
+    if (frame.length > 126) {
+        if (frame.length == 127) {
+            cout << F("WEBSOCKET: Buffer only allows a message with a payload size of ")
+                 << MAX_RECEIVE_MESSAGE_SIZE;
+        } else if (frame.length > 127) {
+            cout << F("WEBSOCKET: Bad payload-length field value: ")
+                 << frame.length;
+        }
+
+        close(WS_PAYLOAD_TOO_BIG, "Maximum payload is 1024 bytes.");
+       
+        return false;
+    }
+
+    // If the length part of the header is 126, it means it contains an extended length field.
+    // Next two bytes contain the actual payload size, so we need to get the "true" length.
+     if (frame.length == 126) {
+        byte exLengthByte1 = client.read();
+        byte exLengthByte2 = client.read();
+        payloadLength = (exLengthByte1 << 8) + exLengthByte2;
+     } 
+     // If frame length is less than 126, that is the size of the payload.
+     else {
+        payloadLength = frame.length;        
+     }
+
+     // Check if our buffer can store the payload.
+     if (payloadLength > MAX_RECEIVE_MESSAGE_SIZE) {
+        cout << F("WEBSOCKET: The frame has a payload size of ")
+             << payloadLength << F(", but buffer size can only store ")
+             << MAX_RECEIVE_MESSAGE_SIZE << F(" bytes.\n");
+
+        close(WS_PAYLOAD_TOO_BIG, "Maximum payload is 2048 bytes.");
+        
+        return false;
+     }
+
+    // Client should always send mask, but check just to be sure    
     if (frame.isMasked) {
         frame.mask[0] = client.read();
         frame.mask[1] = client.read();
@@ -178,54 +230,62 @@ bool WebSocket::getFrame() {
     }
     
     // Get message bytes and unmask them if necessary
-    for (int i = 0; i < frame.length; i++) {
+    for (int i = 0; i < payloadLength; i++) {
         if (frame.isMasked) {
             frame.data[i] = client.read() ^ frame.mask[i % 4];
         } else {
             frame.data[i] = client.read();
         }
     }
-    
-    //
-    // Frame complete!
-    //
-    
-    if (!frame.isFinal) {
-        // We don't handle fragments! Close and disconnect.
-        #ifdef DEBUG
-            Serial.println("Non-final frame, doesn't handle that.");
-        #endif
-        client.print((uint8_t) 0x08);
-        client.write((uint8_t) 0x02);
-        client.write((uint8_t) 0x03);
-        client.write((uint8_t) 0xf1);
-        return false;
-    }
 
     switch (frame.opcode) {
-        case 0x01: // Txt frame
+        
+        case FRAME_OP_PONG:            
+            if (onHeartbeatResponse && (strstr(frame.data, HEARTBEAT_MESSAGE) != NULL)) {            
+                onHeartbeatResponse(*this);
+            }
+            break;
+
+        case FRAME_OP_TEXT:
             // Call the user provided function
-            if (onData)
-                onData(*this, frame.data, frame.length);
+            if (onData) {
+                onData(*this, frame.data, payloadLength);
+            }
             break;
             
-        case 0x08:
-            // Close frame. Answer with close and terminate tcp connection
-            // TODO: Receive all bytes the client might send before closing? No?
-            #ifdef DEBUG
-                Serial.println("Close frame received. Closing in answer.");
-            #endif
-            client.write((uint8_t) 0x08);
-            return false;
+        case FRAME_OP_CLOSE:
+
+            // If we are already closing, the received close frame is hopefully a reply to a previous
+            // close frame we sent. 
+            // TODO: Check that the received close frame is a copy of ours.
+            if (state == CLOSING) {
+                disconnect();
+                cout << F("WEBSOCKET: Close response received. Terminating connection.\n");    
+            }
+            // In this case, the client wants to close, so we echo back the close frame and 
+            // disconnect.     
+            else {
+                cout << F("WEBSOCKET: Close frame received. Closing in answer.\n");                
+                // TODO: How do we echo back the close frame?
+                // First two bytes in payload is a two-byte close code.
+                uint16_t closeStatusCode = frame.data[0] << 8 | frame.data[1];
+                close(closeStatusCode, "");
+                disconnect();
+            }
+            
             break;
+
+        //Unhandled
+        case FRAME_OP_CONT:
+        case FRAME_OP_BINARY:
+        case FRAME_OP_PING:        
+            cout << F("WEBSOCKET: Unsupported frame.\n");
+            return false;
             
         default:
-            // Unexpected. Ignore. Probably should blow up entire universe here, but who cares.
-    		#ifdef DEBUG
-        		Serial.println("Unhandled frame ignored.");
-    		#endif
+            // Unexpected.
+    		cout << F("WEBSOCKET: Unhandled frame.\n");
 			return false;
-            break;
     }
     return true;
 }
@@ -240,21 +300,91 @@ void WebSocket::registerDataCallback(DataCallback *callback) {
 void WebSocket::registerDisconnectCallback(Callback *callback) {
     onDisconnect = callback;
 }
+void WebSocket::registerHeartbeatResponseCallback(Callback *callback) {
+    onHeartbeatResponse = callback;
+}
 
-
-bool WebSocket::send(char *data, byte length) {
-	if (state == CONNECTED) {
-        server.write((uint8_t) 0x81); // Txt frame opcode
-        server.write((uint8_t) length); // Length of data
-        for (int i = 0; i < length ; i++) {
-            server.write(data[i]);
+void WebSocket::close(uint16_t statusCode, char *reason) {    
+    
+    client.write(FRAME_OP_CLOSE);
+    if (statusCode) {
+        client.write(statusCode);
+    }
+    if (reason) {
+        for (int i = 0; i < strlen(reason); i++) {
+            client.write(reason[i]);            
         }
-		delay(1);
+    }
+    state = CLOSING;
+    cout << F("Close frame sent.\n");
+}
+
+void WebSocket::gracelessClose(uint16_t statusCode, char *reason) {
+    close(statusCode, reason);
+    disconnect();
+    cout << F("Connection terminated without waiting for close-frame reply from client.\n");
+}
+
+bool WebSocket::sendMessage(char *payload, uint16_t payloadLength) {
+    send(FRAME_OP_TEXT, payload, payloadLength);
+}
+
+bool WebSocket::sendHeartbeat() {
+    ping(HEARTBEAT_MESSAGE, strlen(HEARTBEAT_MESSAGE));
+}
+
+
+bool WebSocket::send(uint8_t opCode, char *payload, uint16_t payloadLength) {
+    if (state == CONNECTED) {
+        //
+        // First byte, always FIN, as we don't support anything else.
+        //
+        server.write((uint8_t)FRAME_FLAG_FIN | opCode); 
+        
+        //
+        // Second byte, mask and length of payload.            
+        //
+        
+        // If no payload, make sure the length of the payload is 0.
+        if (payload == NULL) {
+            payloadLength = 0;
+        }
+
+        // If payload size is 125 or less, that is the length we add in the  payload-length header field.
+        if (payloadLength <= 125) {
+            server.write((uint8_t) payloadLength);
+        }
+        // If payload size is between 126 and 65535, 126 is the value to add in the payload-length header field.
+        // In this case, the following 2 bytes in the header contain the  actual payload length. 
+        else if ((payloadLength >= 126) && (payloadLength <= 65535)) {
+            server.write(126);
+            byte exLengthByte2 = (byte)(payloadLength & 0xFF);
+            byte exLengthByte1 = (byte)((payloadLength >> 8) & 0xFF);            
+            server.write(exLengthByte1);
+            server.write(exLengthByte2);
+        }
+        // We definately don't handle messages that are bigger than 65535.
+        else {
+            cout << F("WEBSOCKET: Message is too big to send.")
+                 << F("Only messages up to 65535 bytes in size are supported.\n");            
+            return false;
+        }                
+        
+        //
+        // Header is finished, now we write the actual payload.
+        //
+        for (int i = 0; i < payloadLength ; i++) {
+            server.write(payload[i]);            
+        }
         return true;
     }
-#ifdef DEBUG
-    Serial.println("No connection to client, no data sent.");
-#endif
-	
+    cout << F("WEBSOCKET: No connection to client, no data sent.\n");   
     return false;
+}
+
+void WebSocket::ping(char *payload, uint16_t payloadLength) {
+    send(FRAME_OP_PING, payload, payloadLength);
+}
+void WebSocket::pong(char *payload, uint16_t payloadLength) {
+    send(FRAME_OP_PONG, payload, payloadLength);
 }
--- WebSocket.h
+++ WebSocket.h
@@ -36,69 +36,109 @@ http://datatracker.ietf.org/doc/rfc6455/?include_text=1
 
 Modified by Alexandros Baltas, 2013
 www.codebender.cc
+-------------
 
+Modified by Axis Communications, 2014.
 */
 
 #include <Arduino.h> // Arduino 1.0 or greater is required
 #include <stdlib.h>
-
 #include <SPI.h>
 #include <Ethernet.h>
 
 #ifndef WEBSOCKET_H_
 #define WEBSOCKET_H_
 
-// CRLF characters to terminate lines/handshakes in headers.
-#define CRLF "\r\n"
+#define CRLF "\r\n" // CRLF characters to terminate lines/handshakes in headers.
+
+// Status codes for close frames; see spec for details.
+#define WS_NORMAL_CLOSE 1000
+#define WS_ENDPOINT_GOING_AWAY 1001
+#define WS_PROTOCOL_ERROR 1002
+#define WS_PAYLOAD_NOT_ACCEPTABLE 1003
+#define WS_RESERVED 1004
+#define WS_RESERVED_NO_STATUS_CODE 1005
+#define WS_RESERVED_ABNORMAL_CLOSE 1006
+#define WS_MALFORMED_PAYLOAD 1007
+#define WS_POLICY_VIOLATION 1008
+#define WS_PAYLOAD_TOO_BIG 1009
+#define WS_EXTENSION_REQUIRED 1010
+#define WS_UNEXPECTED_CONDITION 1011
+#define WS_RESERVED_TLS_FAILURE 1015
+
+// Frame header flags.
+#define FRAME_FLAG_FIN 0x80 // FIN bit. Is this the final fragment of a multi-fragment message?
+
+//Frame header opcodes.
+#define FRAME_OP_CONT 0x00 // Continuation frame
+#define FRAME_OP_TEXT 0x01 // Text frame
+#define FRAME_OP_BINARY 0x02 // Binary frame
+#define FRAME_OP_CLOSE 0x08 // Close connection
+#define FRAME_OP_PING 0x09 // Ping frame
+#define FRAME_OP_PONG 0x0A // Pong frame
+#define FRAME_OP_BITMASK 0x0F // Bit mask for opcodes
+
+#define MAX_RECEIVE_MESSAGE_SIZE 256
+
+#define HEARTBEAT_MESSAGE "Heartbeat"
 
 class WebSocket {
-public:
-    // Constructor.
-    WebSocket(const char *urlPrefix = "/", int inPort = 80);
-    
-    // Callback functions definition.
-    typedef void DataCallback(WebSocket &socket, char* socketString, byte frameLength);
-    typedef void Callback(WebSocket &socket);
-    
-    // Start tlistening for connections.
-    void begin();
-    
-    // Main listener for incoming data. Should be called from the loop.
-    void listen();
-    
-    // Callbacks
-    void registerDataCallback(DataCallback *callback);
-    void registerConnectCallback(Callback *callback);
-    void registerDisconnectCallback(Callback *callback);
-    
-	// Are we connected?
-	bool isConnected();
-	
-    // Embeds data in frame and sends to client.
-    bool send(char *str, byte length);
+    public:
+        // Constructor.
+        WebSocket(const char *urlPrefix = "/", int inPort = 80);
+        
+        // Callback functions definition.
+        typedef void DataCallback(WebSocket &socket, char* socketString, unsigned short frameLength);
+        typedef void Callback(WebSocket &socket);
+        
+        // Start listening for connections.
+        void begin();
+        
+        // Main listener for incoming data. Should be called from the loop.
+        void listen();
+        
+        void close(uint16_t, char*);
+        void gracelessClose(uint16_t, char*);
 
-private:
-    EthernetServer server;
-    EthernetClient client;
-    enum State {DISCONNECTED, CONNECTED} state;
+        // Callbacks
+        void registerDataCallback(DataCallback *callback);
+        void registerConnectCallback(Callback *callback);
+        void registerDisconnectCallback(Callback *callback);
+        void registerHeartbeatResponseCallback(Callback *callback);
+        
+    	// Are we connected?
+    	bool isConnected();
+    	
+        bool sendMessage(char*, uint16_t);
+        bool sendHeartbeat();
 
-    const char *socket_urlPrefix;
+        void ping(char*, uint16_t);
+        void pong(char*, uint16_t);
+        bool send(uint8_t opCode, char*, uint16_t);
 
-    // Pointer to the callback function the user should provide
-    DataCallback *onData;
-    Callback *onConnect;
-    Callback *onDisconnect;
+    private:
+        EthernetServer server;
+        EthernetClient client;
+        enum State {DISCONNECTED, CONNECTED, CLOSING} state;
 
-    // Discovers if the client's header is requesting an upgrade to a
-    // websocket connection.
-    bool doHandshake();
-    
-    // Reads a frame from client. Returns false if user disconnects, 
-    // or unhandled frame is received. Server must then disconnect, or an error occurs.
-    bool getFrame();
+        const char *socket_urlPrefix;
 
-    // Disconnect user gracefully.
-    void disconnectStream();
+        // Pointer to the callback function the user should provide
+        DataCallback *onData;
+        Callback *onConnect;
+        Callback *onDisconnect;
+        Callback *onHeartbeatResponse;
+
+        // Discovers if the client's header is requesting an upgrade to a
+        // websocket connection.
+        bool processHandshake();
+        
+        // Reads a frame from client. Returns false if user disconnects, 
+        // or unhandled frame is received. Server must then disconnect, or an error occurs.
+        bool getFrame();
+
+        // Disconnect user gracefully.
+        void disconnect();
 };
 
 #endif
